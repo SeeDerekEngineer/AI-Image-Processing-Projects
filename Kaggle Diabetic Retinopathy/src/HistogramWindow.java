0e0833f15add0bf647518f9da4395142ff3cb108
Derek Thayer <dthayerstudent@gmail.com>
2015-06-04 20:38:00 -0400

    Added HistogramWindow Class

---
+++ Kaggle Diabetic Retinopathy/src/HistogramWindow.java
@@ -0,0 +1,25 @@
+
+public class HistogramWindow {
+
+	private static int width = 100;
+	private static int height = 100;
+	private static int increment = 20;
+	private int x = 0;
+	private int y = 0;
+	
+	public int getWidth(){
+		return width;
+	}//End getWidth method
+
+	public int getHeight(){
+		return height;
+	}//End getHeight method
+
+	public void setX(int newX){
+		this.x = newX;
+	}//End setX method
+
+	public void setY(int newY){
+		this.y = newY;
+	}//End setY method
+}
